d8385d66221fd5506938e40c6e56506b0073fe0f
Amos Oluoch <aoluoch271@gmail.com>
2024-07-07 21:42:18 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li')
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = 'remove'
+  p.textContent = `${todo} ` 
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector('#list').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
